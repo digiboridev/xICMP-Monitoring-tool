9421b63150db652ce91e53d854b635f30cb409b0
digiborimusik <digiborimusik@gmail.com>
2023-11-30 23:13:01 +0200

    Fix process spam janks using isolate

--- lib/data/service/monitoring.dart
+++ lib/data/service/monitoring.dart
@@ -1,5 +1,8 @@
+// ignore_for_file: unused_import
+
 import 'dart:async';
 import 'dart:io';
+import 'dart:isolate';
 
 import 'package:rxdart/streams.dart';
 import 'package:xicmpmt/core/app_logger.dart';
@@ -15,32 +18,18 @@ class MonitoringService {
   StreamSubscription? _monitoringSubscription;
 
   upsertMonitoring() async {
+    AppLogger.debug('upsertMonitoring: ${_monitoringSubscription != null}', name: 'MonitoringService');
     _monitoringSubscription?.cancel();
     _monitoringSubscription = _createMonitoringStream().listen((ping) => _repository.setPing(ping));
   }
 
-  Stream<Ping> _createMonitoringStream({Duration interval = const Duration(milliseconds: 564)}) async* {
-    //
-    // Pseudo-parrallel
-    //
-    // while (true) {
-    //   List<Host> hosts = await _repository.getAllHosts();
-    //   AppLogger.debug('Hosts: $hosts', name: 'MonitoringService');
-    //   Iterable<Future<Ping>> jobs = hosts.where((element) => element.enabled).map((e) => _pingTo(e.adress));
-    //   List<Ping> pings = await Future.wait(jobs);
-    //   for (Ping ping in pings) {
-    //     yield ping;
-    //   }
-    //   AppLogger.debug('Pings: $pings', name: 'MonitoringService');
-    //   await Future.delayed(interval);
-    // }
+  Stream<Ping> _createMonitoringStream({Duration interval = const Duration(milliseconds: 64)}) async* {
+    List<Host> hosts = await _repository.getAllHosts();
+    AppLogger.debug('Hosts: $hosts', name: 'MonitoringService');
 
-    //
     // Sequential
     //
     // while (true) {
-    //   List<Host> hosts = await _repository.getAllHosts();
-    //   AppLogger.debug('Hosts: $hosts', name: 'MonitoringService');
     //   for (Host host in hosts) {
     //     Ping ping = await _pingTo(host.adress);
     //     yield ping;
@@ -48,26 +37,57 @@ class MonitoringService {
     //   await Future.delayed(interval);
     // }
 
+    // Pseudo-parallel
     //
-    // Parallel
+    // while (true) {
+    //   Iterable<Future<Ping>> jobs = hosts.where((element) => element.enabled).map((e) => _pingTo(e.adress));
+    //   List<Ping> pings = await Future.wait(jobs);
+    //   for (Ping ping in pings) {
+    //     yield ping;
+    //   }
+    //   await Future.delayed(interval);
+    // }
+
+    // Parallel-merged
     //
-    List<Host> hosts = await _repository.getAllHosts();
-    AppLogger.debug('Hosts: $hosts', name: 'MonitoringService');
-    List<Stream<Ping>> streams = [];
-    for (Host host in hosts) {
-      if (!host.enabled) continue;
-      streams.add(Stream.periodic(interval, (i) => i).asyncMap((i) => _pingTo(host.adress)));
-    }
-    await for (Ping ping in MergeStream(streams)) {
-      yield ping;
-      // Print every ~100th ping
-      if (DateTime.now().microsecondsSinceEpoch % 100 == 1) {
-        AppLogger.debug('$ping', name: 'MonitoringService');
+    // List<Stream<Ping>> streams = [];
+    // for (Host host in hosts) {
+    //   if (!host.enabled) continue;
+    //   streams.add(Stream.periodic(interval, (i) => i).asyncMap((i) => _pingTo(host.adress)));
+    // }
+    // await for (Ping ping in MergeStream(streams)) {
+    //   yield ping;
+    // }
+
+    // Parallel-isolated
+    //
+    late Isolate isolate;
+    try {
+      final receivePort = ReceivePort();
+      isolate = await Isolate.spawn(
+        (port) async {
+          List<Stream<Ping>> streams = [];
+          for (Host host in hosts) {
+            if (!host.enabled) continue;
+            streams.add(Stream.periodic(interval, (i) => i).asyncMap((i) => _pingTo(host.adress)));
+          }
+          for (Stream<Ping> stream in streams) {
+            stream.listen((ping) => port.send(ping));
+          }
+        },
+        receivePort.sendPort,
+      );
+
+      await for (var msg in receivePort) {
+        if (msg is Ping) yield msg;
       }
+    } finally {
+      AppLogger.debug('finally', name: 'MonitoringService');
+      isolate.kill(priority: Isolate.immediate);
     }
   }
 
-  Future<Ping> _pingTo(String adress, {int limit = 1000}) async {
+  static Future<Ping> _pingTo(String adress, {int limit = 1000}) async {
     final sendTime = DateTime.now();
     try {
       final proc = await Process.run('ping', ['-c', '1', adress]).timeout(Duration(milliseconds: limit));
