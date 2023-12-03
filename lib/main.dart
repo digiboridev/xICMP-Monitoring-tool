80200a7fecc3884efc0c25606cd344101122c3d6
digiborimusik <digiborimusik@gmail.com>
2023-12-03 16:57:26 +0200

    sentry integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,43 @@
 // ignore_for_file: unused_import
 import 'dart:developer';
 import 'package:flutter/material.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 // import 'package:flutter/rendering.dart';
 import 'package:xicmpmt/core/app_logger.dart';
 import 'package:xicmpmt/screens/main_screen.dart';
 
-void main() {
+Future<void> main() async {
   // debugRepaintRainbowEnabled = true;
+
   AppLogger.stream.listen((LogEntity l) {
     // Setup local log
     log(l.msg, time: l.time, error: l.error, stackTrace: l.stack, name: l.name, level: l.level.index);
 
     // Setup remote log
-    // TODO: implement remote log
+    if (l.error != null) Sentry.captureException(l.error, stackTrace: l.stack);
+    if (l.level == Level.info) Sentry.captureMessage(l.msg, template: l.name);
+    final b = Breadcrumb(message: l.msg, level: SentryLevel.fromName(l.level.name), timestamp: l.time, category: l.name);
+    Sentry.addBreadcrumb(b);
   });
-  runApp(const PingStatsApp());
+
+  // runApp(const App());
+  await SentryFlutter.init(
+    (options) {
+      options.dsn = const String.fromEnvironment('sentryKey');
+      options.tracesSampleRate = 1.0;
+      options.enablePrintBreadcrumbs = false;
+      options.enableAutoPerformanceTracing = true;
+      // options.beforeSendTransaction = (transaction) async {
+      //   debugPrint('tr send: ${transaction.eventId} ${DateTime.now()}');
+      //   return transaction;
+      // };
+    },
+    appRunner: () => runApp(const App()),
+  );
 }
 
-class PingStatsApp extends StatelessWidget {
-  const PingStatsApp({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -43,4 +62,4 @@ class PingStatsApp extends StatelessWidget {
 }
 
 
- //  TODO: sentry, about, sign, md, policy, license, release.
+ //  TODO: about, sign, md, policy, license, release.
